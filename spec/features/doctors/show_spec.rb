f81e8e4e2c0d63bef8e5d19215cf79ede5b16ba1
Alex M <alexm0717@hotmail.com>
2022-09-26 09:27:49 -0700

    end of story one

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -7,13 +7,16 @@ RSpec.describe("Doctors Show page") do
     @doc1 = @hospital1.doctors.create!(    name: "A",     specialty: "aliving ppl",     university: "BCC")
     @doc2 = @hospital1.doctors.create!(    name: "B",     specialty: "Healing",     university: "NY")
     @patient1 = Patient.create!(    name: "Pat1",     age: 25)
+    @docpat1 = Doctorpatient.create!(    doctor_id: @doc1.id,     patient_id: @patient1.id)
   end
 
-    #@docpat1 = Doctorpatients.create!(    doctor_id: @doc1.id,     patient_id: @patient1.id)
   it("I see all of that doctor's information, name,specialty,university") do
     visit("/doctors/#{@doc1.id}")
+    save_and_open_page
     expect(page).to(have_content("Name:#{@doc1.name}"))
     expect(page).to(have_content("Specialty:#{@doc1.specialty}"))
     expect(page).to(have_content("University:#{@doc1.university}"))
+    expect(page).to(have_content("Works at hospital:#{@doc1.hospital.name}"))
+    expect(page).to(have_content("Patients names:#{@patient1.name}"))
   end
 end
